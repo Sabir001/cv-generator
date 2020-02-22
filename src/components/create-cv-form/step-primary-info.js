45a5d70a20b485734e06a32178bd9834933c1732
MD. Jamil Uddin <jamilbd07@gmail.com>
2020-02-23 00:52:10 +0600

    form validation

--- src/components/create-cv-form/step-primary-info.js
+++ src/components/create-cv-form/step-primary-info.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
 const PrimaryForm = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    const [name, setName] = useState({name: '', error: [] });
+    const [email, setEmail] = useState({email: '', error: [] });
     const [phone, setPhone] = useState('');
     const [links, setLinks] = useState([{name: "", url: ""}]);
     const [submit, setSubmit] = useState(false);
 
-    const handleName = (event) => {
-        setName(event.target.value);
+    const handleName = (event) => {        
+        setName({name: event.target.value, error: {}});
     };
 
     const handleEmail = (event) => {
-        setEmail(event.target.value);
+        setEmail({email: event.target.value, error: {}});
     };
 
     const handlePhone = (event) => {
@@ -34,6 +34,39 @@ const PrimaryForm = () => {
         setLinks(newLink);
     };
 
+    //Empty Validation Function
+    const emptyCheck = (value) => {
+        if (value.length < 1 ) {
+            return false;
+        }
+        return true;
+    };
+
+    //Length Validation Check
+    const lengthCheck = (value, minlength, maxLength) => {
+        if (value.length <= minlength && value.length <= maxLength ) {
+            console.log("ddd");
+            return false;
+        }
+        return true;
+    };
+
+    //Name Validation Check
+    const nameCheck = (name) => {
+        if(/[!@#$%^&*(),.?":{}|<>]/g.test(name) || /\d+/g.test(name)) {
+            return false;
+        }
+        return true;
+    };
+
+    //Email Validation Check
+    const emailCheck = (email) => {
+        if (!(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email))) {
+            return false;
+        }
+        return true;
+    }
+
     const addLink = (e) => {
         e.preventDefault();
         setLinks([...links, { name: "", url: "" }]);
@@ -46,19 +79,53 @@ const PrimaryForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setSubmit(true);
+        let submit = true;
+
+        //Name Validation
+        let nameError = [];
+        if ( !emptyCheck(name.name) ) {
+            submit = false;
+            nameError = [...nameError, 'Name shouldn\'t be empty.'];
+        }
+        if ( !lengthCheck(name.name, 4, 30) ) {
+            submit = false;
+            nameError = [...nameError, 'Name should be minimum 4 and maximum 30 words.'];
+        }
+        if ( !nameCheck(name.name) ) {
+            submit = false;
+            nameError = [...nameError, 'The name you provided is not valid.'];
+        }
+        setName({name: name.name, error: nameError });
+
+        //Email Validation
+        let emailError = [];
+        if ( !emptyCheck(email.email) ) {
+            submit = false;
+            emailError = [...emailError, 'Email shouldn\'t be empty.'];
+        }
+        if ( !emailCheck(email.email) ) {
+            submit = false;
+            emailError = [...emailError, 'The Email you provided is not valid.'];
+        }
+        setEmail({email: email.email, error: emailError });
+
+        if (submit) {
+            setSubmit(true);
+        }        
     };
     
+    console.log("Name ", name);
+    console.log("Email ", email);
     return (
         <div className="personal-info">
             <div>
                 <label name="name">Name</label>
-                <input type="text" name="name" value={name} onChange={event => handleName(event)} placeholder="Your Name" /><br/>
+                <input type="text" name="name" value={name.name} onChange={event => handleName(event)} placeholder="Your Name" /><br/>
             </div>
 
             <div>
                 <label name="email">Email</label>
-                <input type="email" name="email" value={email} onChange={event => handleEmail(event)} placeholder="Your Email" /><br/>
+                <input type="email" name="email" value={email.email} onChange={event => handleEmail(event)} placeholder="Your Email" /><br/>
             </div>
 
             <div>
@@ -92,11 +159,11 @@ const PrimaryForm = () => {
             {submit && 
                 <div>
                     { name && 
-                        <p>Name: {name}</p>
+                        <p>Name: {name.name}</p>
                     }
 
                     { email && 
-                        <p>Email: {email}</p>
+                        <p>Email: {email.email}</p>
                     }
 
                     { phone && 
