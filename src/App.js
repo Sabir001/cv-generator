9a5f1685bb4e37dcbab9f90ad3e4145542345595
sabir001 <asabir24@gmail.com>
2020-02-26 17:39:59 +0600

    Unnecessary test components removed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { createGlobalStyle } from "styled-components";
 import LandingMain from "./components/landing-page/main";
 import CreateCVForm from "./components/create-cv-form/create-form";
-import Test from "./test";
-import TestFunc from "./components/testFunc";
 
 import {
   AppContainer,
