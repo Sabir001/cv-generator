2957933dad66c776ed4a6212865617440cf490dc
sabir001 <asabir24@gmail.com>
2020-02-26 17:37:21 +0600

    Unnecessary css files removed

--- src/App.js
+++ src/App.js
@@ -23,7 +23,13 @@ const GlobalStyle = createGlobalStyle`
    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
    -webkit-font-smoothing: antialiased;
    -moz-osx-font-smoothing: grayscale;
+   margin: 0;
  }
+ code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  }
+
 `;
 
 function App() {
